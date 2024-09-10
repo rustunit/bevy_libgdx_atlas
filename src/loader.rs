2798766625c36a283a01982c70df66e892f7b9b2
TrialDragon <31419708+TrialDragon@users.noreply.github.com>
2024-09-09 19:05:49 -0700

    Update to Bevy 0.14 (#2)

--- src/loader.rs
+++ src/loader.rs
@@ -17,47 +17,48 @@ impl AssetLoader for LibGdxAtlasAssetLoader {
         &["libgdx.atlas"]
     }
 
-    fn load<'a>(
+    async fn load<'a>(
         &'a self,
-        reader: &'a mut bevy::asset::io::Reader,
+        reader: &'a mut bevy::asset::io::Reader<'_>,
         _settings: &'a Self::Settings,
-        load_context: &'a mut bevy::asset::LoadContext,
-    ) -> bevy::utils::BoxedFuture<'a, Result<Self::Asset, Self::Error>> {
-        Box::pin(async move {
-            let mut file = String::new();
-            reader.read_to_string(&mut file).await?;
+        load_context: &'a mut bevy::asset::LoadContext<'_>,
+    ) -> Result<Self::Asset, Self::Error> {
+        let mut file = String::new();
+        reader.read_to_string(&mut file).await?;
 
-            let asset = AssetFile::new(file)?;
+        let asset = AssetFile::new(file)?;
 
-            let image: Image = load_context
-                .load_direct(
-                    load_context
-                        .asset_path()
-                        .path()
-                        .parent()
-                        .unwrap()
-                        .join(asset.file),
-                )
-                .await?
-                .take()
-                .expect("expected image asset");
+        let path = load_context
+            .asset_path()
+            .path()
+            .parent()
+            .unwrap()
+            .join(asset.file);
 
-            let mut layout = TextureAtlasLayout::new_empty(asset.size.as_vec2());
-            let mut files = HashMap::new();
+        let image: Image = load_context
+            .loader()
+            .direct()
+            .untyped()
+            .load(path)
+            .await?
+            .take()
+            .expect("expected image asset");
 
-            for frame in asset.files {
-                let id = layout.add_texture(frame.bounds);
-                files.insert(frame.filename, id);
-            }
+        let mut layout = TextureAtlasLayout::new_empty(asset.size.as_uvec2());
+        let mut files = HashMap::new();
 
-            let atlas = load_context.add_labeled_asset("atlas_layout".into(), layout);
-            let image = load_context.add_labeled_asset("atlas_texture".into(), image);
+        for frame in asset.files {
+            let id = layout.add_texture(frame.bounds.as_urect());
+            files.insert(frame.filename, id);
+        }
 
-            Ok(LibGdxAtlasAsset {
-                image,
-                atlas,
-                files,
-            })
+        let atlas = load_context.add_labeled_asset("atlas_layout".into(), layout);
+        let image = load_context.add_labeled_asset("atlas_texture".into(), image);
+
+        Ok(LibGdxAtlasAsset {
+            image,
+            atlas,
+            files,
         })
     }
 }
