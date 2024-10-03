ded71f0e1052c66ae5e0fec6da8663978155249c
TrialDragon <31419708+TrialDragon@users.noreply.github.com>
2024-10-03 05:19:00 -0700

    Add animated example (#3)

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Support loading `libgdx.atlas` files (used for sprite sheets and such) as Bevy a
 
 ## Usage
 
+Pack your spritesheet using https://github.com/crashinvaders/gdx-texture-packer-gui
+
+<img src="texture_packer_example.webp" width="800" height="450" alt="Texture packer example" />
+
 Add the `LibGdxAssetPlugin` to your app:
 
 ```rust
@@ -23,16 +27,10 @@ app.add_plugins(LibGdxAssetPlugin);
 
 Now when you load files with the `.libgdx.atlas` extension through the asset server, or even `bevy_asset_loader`, they will load as a `LibGdxAtlasAsset` which you can then use.
 
-## Compatible Bevy Versions
+<img src="animated_spritesheet_example.webp" width="800" height="450" alt="Animated spritesheet example" />
 
-|bevy|bevy_libgdx_atlas|
-|-|-|
-|0.14|0.2|
-|0.13|0.1|
-
-## License
-
-bevy_libgdx_atlas is dual-licensed under either [MIT](https://opensource.org/license/MIT) or [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0), at your option.
+> [!TIP]
+> Run `cargo run --example animation` to see this example for yourself!
 
 ## Contributing
 
@@ -48,3 +46,14 @@ bevy_libgdx_atlas is dual-licensed under either [MIT](https://opensource.org/lic
 - [bevy_ios_notifications](https://github.com/rustunit/bevy_ios_notifications)
 - [bevy_ios_gamecenter](https://github.com/rustunit/bevy_ios_gamecenter)
 - [bevy_web_popups](https://github.com/rustunit/bevy_web_popups)
+
+## Compatible Bevy Versions
+
+|bevy|bevy_libgdx_atlas|
+|-|-|
+|0.14|0.2|
+|0.13|0.1|
+
+## License
+
+bevy_libgdx_atlas is dual-licensed under either [MIT](https://opensource.org/license/MIT) or [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0), at your option.
---
+++ examples/animation.rs
@@ -0,0 +1,117 @@
+use bevy::{prelude::*, render::camera::ScalingMode, window::WindowResolution};
+use bevy_libgdx_atlas::*;
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Bevy LibGDX Atlas Animation Example".to_string(),
+                    // So that the resolution matches the sprites aspect ratio.
+                    resolution: WindowResolution::new(512., 512.),
+                    ..default()
+                }),
+                ..default()
+            })
+            // To allow the pixel art to be clear.
+            .set(ImagePlugin::default_nearest()),
+    );
+    // Needed to initialize the basics of the LibGdxAssets.
+    app.add_plugins(LibGdxAssetPlugin);
+    app.init_state::<ExampleState>();
+    app.init_resource::<AnimationSheetHandle>();
+    // This, and the check_if_loaded system, are to allow for
+    // the assets to be loaded, since we can't use them immediately.
+    app.add_systems(OnEnter(ExampleState::Loading), load_assets);
+    app.add_systems(
+        Update,
+        check_if_loaded.run_if(in_state(ExampleState::Loading)),
+    );
+    app.add_systems(OnEnter(ExampleState::Loaded), setup);
+    app.add_systems(Update, animate_sheet.run_if(in_state(ExampleState::Loaded)));
+    app.run();
+}
+
+#[derive(States, Hash, Debug, Default, Clone, Eq, PartialEq)]
+enum ExampleState {
+    #[default]
+    Loading,
+    Loaded,
+}
+
+#[derive(Resource, Default, Debug)]
+struct AnimationSheetHandle(Handle<LibGdxAtlasAsset>);
+
+fn load_assets(mut handle: ResMut<AnimationSheetHandle>, asset_server: Res<AssetServer>) {
+    handle.0 = asset_server.load("animation_sheet.libgdx.atlas");
+}
+
+fn check_if_loaded(
+    handle: Res<AnimationSheetHandle>,
+    atlases: Res<Assets<LibGdxAtlasAsset>>,
+    mut next_state: ResMut<NextState<ExampleState>>,
+) {
+    if atlases.get(&handle.0).is_some() {
+        next_state.set(ExampleState::Loaded);
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    handle: Res<AnimationSheetHandle>,
+    atlases: Res<Assets<LibGdxAtlasAsset>>,
+) {
+    // Get the LibGDX atlas asset for however we wish to use it.
+    let animation_sheet = atlases.get(&handle.0).unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            texture: animation_sheet.image.clone(),
+            ..default()
+        },
+        TextureAtlas {
+            layout: animation_sheet.atlas.clone(),
+            index: 0,
+        },
+        AnimationConfig {
+            first_index: 0,
+            last_index: animation_sheet.files.len().saturating_sub(1),
+            timer: Timer::from_seconds(0.1, TimerMode::Repeating),
+        },
+    ));
+
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scaling_mode = ScalingMode::Fixed {
+        width: 16.,
+        height: 16.,
+    };
+    commands.spawn(camera);
+}
+
+#[derive(Component)]
+struct AnimationConfig {
+    first_index: usize,
+    last_index: usize,
+    timer: Timer,
+}
+
+fn animate_sheet(time: Res<Time>, mut query: Query<(&mut AnimationConfig, &mut TextureAtlas)>) {
+    for (mut config, mut atlas) in &mut query {
+        config.timer.tick(time.delta());
+
+        // Early return if it isn't time for the next
+        // step or frame in the animation.
+        if !config.timer.just_finished() {
+            return;
+        }
+
+        // Restart the animation to the first index if
+        // it has reached the last index.
+        atlas.index = if atlas.index == config.last_index {
+            config.first_index
+        } else {
+            atlas.index + 1
+        };
+    }
+}
